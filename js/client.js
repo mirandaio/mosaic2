94a9078ae937d70eb69aefe6eff8e5bddea2f491
Alfredo Miranda <alfredo@miranda.io>
2016-03-20 19:09:39 -0500

    Add drag and drop functionality

--- js/client.js
+++ js/client.js
@@ -4,15 +4,41 @@
     var canvas = document.createElement('canvas');
     var container = document.getElementById('container');
     var button = document.getElementById('process');
+    var dropZone = document.getElementById('drop-zone');
 
     document.querySelector('input').addEventListener('change', function(e) {
-      setupImageAndCanvas(e, img, canvas, button);
+      setupImageAndCanvas(e.target.files[0], img, canvas, button, dropZone);
+    });
+
+    dropZone.addEventListener('dragenter', function() {
+      this.classList.remove('empty');
+      this.classList.add('full');
+    });
+
+    dropZone.addEventListener('dragleave', function() {
+      this.classList.remove('full');
+      this.classList.add('empty');
+    });
+
+    dropZone.addEventListener('drop', function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      var file = e.dataTransfer.files[0];
+      setupImageAndCanvas(file, img, canvas, button, dropZone);
     });
 
     button.addEventListener('click', function() {
       processPhoto(canvas, container);
     });
   };
+
+  window.addEventListener('drop', function(e) {
+    e.preventDefault();
+  });
+
+  window.addEventListener('dragover', function(e) {
+    e.preventDefault();
+  });
 
   /**
    * Takes care of processing the uploaded photo. Inserts the rows of the
@@ -35,7 +61,6 @@
     for(y = 0; y < canvasHeight; y += TILE_HEIGHT) {
       rowPromises.push(getRow(imgData, y));
     }
-
     // display the row promises resolved values as soon they become available
     // maintaining order
     rowPromises.reduce(function(sequence, rowPromise) {
@@ -70,7 +95,7 @@
    * Puts the data of the uploaded file into the image element, and into the
    * canvas element
    */
-  function setupImageAndCanvas(e, img, canvas, button) {
+  function setupImageAndCanvas(file, img, canvas, button, dropZone) {
     var reader = new FileReader();
     var context = canvas.getContext('2d');
     reader.onload = function(evt) {
@@ -79,9 +104,10 @@
         canvas.height = img.clientHeight;
         context.drawImage(img, 0, 0, canvas.width, canvas.height);
         button.classList.remove('invisible');
+        dropZone.classList.add('none');
       };
       img.src = evt.target.result;
     };
-    reader.readAsDataURL(e.target.files[0]);
+    reader.readAsDataURL(file);
   }
 })(window, document);
